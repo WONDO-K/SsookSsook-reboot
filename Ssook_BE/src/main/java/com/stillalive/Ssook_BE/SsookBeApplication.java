1f220ab3b3f6f63be36ba11f65bfb84f2f9676ff
김동건 <sky4tony@naver.com>
2024-10-29 16:26:24 +0900

    백엔드 서버 초기 설정 및 스웨거, 환경변수 설정

---
+++ Ssook_BE/src/main/java/com/stillalive/Ssook_BE/SsookBeApplication.java
@@ -0,0 +1,13 @@
+package com.stillalive.Ssook_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsookBeApplication.class, args);
+	}
+
+}
---
+++ Ssook_BE/src/test/java/com/stillalive/Ssook_BE/SsookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stillalive.Ssook_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
