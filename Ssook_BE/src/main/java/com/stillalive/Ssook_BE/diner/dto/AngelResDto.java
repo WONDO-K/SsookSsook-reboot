e81c9a71f3242206cb1f72f82571c5b9cc8481e0
차봉석 <chaqhdtjr7@gmail.com>
2024-11-03 19:39:52 +0900

    [Feat] #3 Diner - 식당 리스트, 상세 조회 API 구현

---
+++ Ssook_BE/src/main/java/com/stillalive/Ssook_BE/diner/dto/AngelResDto.java
@@ -0,0 +1,16 @@
+package com.stillalive.Ssook_BE.diner.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AngelResDto {
+    private Integer dinerId;
+    private String name;
+    private String address;
+    private float lat;
+    private float lng;
+    private String tel;
+    private Boolean isAngel;
+}
--- Ssook_BE/src/main/java/com/stillalive/Ssook_BE/diner/service/DinerService.java
+++ Ssook_BE/src/main/java/com/stillalive/Ssook_BE/diner/service/DinerService.java
@@ -1,13 +1,78 @@
 package com.stillalive.Ssook_BE.diner.service;
 
+import com.stillalive.Ssook_BE.diner.dto.AngelListResDto;
+import com.stillalive.Ssook_BE.diner.dto.AngelResDto;
+import com.stillalive.Ssook_BE.diner.dto.DinerListResDto;
+import com.stillalive.Ssook_BE.diner.dto.DinerResDto;
 import com.stillalive.Ssook_BE.diner.repository.DinerRepository;
+import com.stillalive.Ssook_BE.exception.SsookException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.stillalive.Ssook_BE.exception.ErrorCode;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class DinerService {
 
     private final DinerRepository dinerRepository;
+
+    public DinerListResDto getDinerList() {
+        return DinerListResDto.builder()
+                .dinerList(
+                        dinerRepository.findAll()
+                                .stream()
+                                .map(diner -> DinerResDto.builder()
+                                        .dinerId(diner.getId())
+                                        .name(diner.getName())
+                                        .address(diner.getAddress())
+                                        .lat(diner.getLat())
+                                        .lng(diner.getLng())
+                                        .tel(diner.getTel())
+                                        .isAngel(diner.getIsAngel())
+                                        .build()
+                                )
+                                .collect(Collectors.toList())
+                )
+                .totalItems((int) dinerRepository.count())
+                .build();
+    }
+
+    public AngelListResDto getAngelList() {
+        return AngelListResDto.builder()
+                .angelList(
+                        dinerRepository.findAllByIsAngelTrue()
+                                .stream()
+                                .map(diner -> AngelResDto.builder()
+                                        .dinerId(diner.getId())
+                                        .name(diner.getName())
+                                        .address(diner.getAddress())
+                                        .lat(diner.getLat())
+                                        .lng(diner.getLng())
+                                        .tel(diner.getTel())
+                                        .isAngel(diner.getIsAngel())
+                                        .build()
+                                )
+                                .collect(Collectors.toList())
+                )
+                .totalItems(dinerRepository.countAllByIsAngelTrue())
+                .build();
+    }
+
+    public DinerResDto getDinerDetail(Integer dinerId) {
+        return dinerRepository.findById(dinerId)
+                .map(diner -> DinerResDto.builder()
+                        .dinerId(diner.getId())
+                        .name(diner.getName())
+                        .address(diner.getAddress())
+                        .lat(diner.getLat())
+                        .lng(diner.getLng())
+                        .tel(diner.getTel())
+                        .isAngel(diner.getIsAngel())
+                        .build()
+                )
+                .orElseThrow(() -> new SsookException(ErrorCode.DINER_NOT_FOUND));
+    }
 
 }
