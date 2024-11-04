e098147e4ac195ec77f5b509839b63f1c601710c
최동호 <raspic5874@naver.com>
2024-11-04 16:13:44 +0900

    [Feat] 카카오페이 결제 구현

--- Ssook_BE/src/main/java/com/stillalive/Ssook_BE/pay/controller/PayViewController.java
+++ Ssook_BE/src/main/java/com/stillalive/Ssook_BE/pay/controller/PayViewController.java
@@ -13,16 +13,16 @@ public class PayViewController {
     @GetMapping("/point/charge")
     @Operation(summary = "포인트 충전 페이지", description = "포인트 충전 페이지를 렌더링합니다.")
     public String showPaymentPage(Model model) {
-        model.addAttribute("loginId", "exampleLoginId"); // 예시 로그인 ID
-        model.addAttribute("userId", "12345"); // 예시 사용자 ID
+        model.addAttribute("loginId", "string"); // 예시 로그인 ID
+        model.addAttribute("userId", "1"); // 예시 사용자 ID
         return "payment"; // templates/payment.mustache를 렌더링
     }
 
 //    @GetMapping("/point/charge")
 //    @Operation(summary = "포인트 충전 페이지", description = "포인트 충전 페이지를 렌더링합니다.")
 //    public String showPaymentPage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-//        model.addAttribute("loginId", userDetails.getLoginId()); // 로그인 ID
-//        model.addAttribute("userId", userDetails.getUserId()); // 사용자 ID
+//        model.addAttribute("loginId", userDetails.getUsername()); // 로그인 ID
+//        model.addAttribute("userId", userDetails.getParentId()); // 사용자 ID
 //        return "payment"; // templates/payment.mustache를 렌더링
 //    }
 }
