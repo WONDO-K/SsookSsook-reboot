7326acccd320083edcc06f3e247090ee5fc0f0a9
차봉석 <chaqhdtjr7@gmail.com>
2024-11-12 16:44:10 +0900

    [Feat] Diner - GET /diner - 전체 식당 목록 확인 : 페이지네이션구현

--- Ssook_BE/src/main/java/com/stillalive/Ssook_BE/diner/repository/DinerRepository.java
+++ Ssook_BE/src/main/java/com/stillalive/Ssook_BE/diner/repository/DinerRepository.java
@@ -1,6 +1,8 @@
 package com.stillalive.Ssook_BE.diner.repository;
 
 import com.stillalive.Ssook_BE.domain.Diner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -25,4 +27,6 @@ public interface DinerRepository extends JpaRepository<Diner, Integer> {
     List<Diner> findNearbyDiners(@Param("userLat") double userLat,
                                  @Param("userLng") double userLng,
                                  @Param("range") double range);
+
+    Page<Diner> findAll(Pageable pageable);
 }
